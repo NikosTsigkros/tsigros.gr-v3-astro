1e9ce37300f7b169f013800377626e494680cf63
NikosTsigkros <tsigkrosn@gmail.com>
2026-03-21 02:09:21 +0200

    add mailer functionalities

---
+++ public/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'TSIGROS.GR'"; //letter subject
+	$to = 'contact@tsigros.gr'; // Enter Your E-mail
+	$from = 'webmail@tsigros.gr'; // Admin e-mail
+	$fromName = 'Tsigkros Web Developer'; // Your company name
+	$charset = 'UTF-8';
+?>
